efe16ae81bca022c232f5045471b84303b8d8259
AswathiVV <aswathi.v.v.916@gmail.com>
2025-02-10 13:34:48 +0530

    LE

--- Lunessence/app/models.py
+++ Lunessence/app/models.py
@@ -2,30 +2,47 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import timedelta, date
 
-
-# Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.name
     
 class DestinationWedding(models.Model):
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
     name = models.CharField(max_length=255) 
     about = models.TextField() 
     img = models.FileField()
     location = models.CharField(max_length=255) 
-    # estimated_guests = models.PositiveIntegerField()
-    # event_date = models.DateField()
     package_price = models.DecimalField(max_digits=10, decimal_places=2) 
 
     def __str__(self):
         return self.name
     
+    
+class Category(models.Model):
+    img = models.FileField()
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    img = models.FileField()
+
+    def __str__(self):
+        return self.name    
+  
+class Invitation_Category(models.Model):
+    img = models.FileField()
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name    
+    
 class Buy(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
-    category = models.ForeignKey(Category, on_delete=models.CASCADE) 
+    wedding = models.ForeignKey(DestinationWedding, on_delete=models.CASCADE,null=True) 
+    items = models.ForeignKey(Item, on_delete=models.CASCADE, null=True)
     customer_name = models.CharField(max_length=255)  
     customer_email = models.EmailField()
     customer_phone = models.CharField(max_length=15)  
@@ -38,4 +55,13 @@ class Buy(models.Model):
     )      
     def is_cancellable(self):
         return (date.today() - self.purchase_date) <= timedelta(days=2)
+    
+    def calculate_total(self):
+        total_price = sum(buy_item.item.price * buy_item.quantity for buy_item in self.buyitem_set.all())
+        return total_price
+
+
+
+
+
     
